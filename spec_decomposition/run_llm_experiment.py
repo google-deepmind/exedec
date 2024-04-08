8ff1af4d126363cc1ef8704e059d5336107406fe
Kensen Shi <kshi@google.com>
2024-04-08 15:07:46 -0700

    initial ExeDec move
    PiperOrigin-RevId: 622963241
    Change-Id: Iad0291c0e0ab2fe8cc326f66f0b33921ac1d046b

---
+++ spec_decomposition/run_llm_experiment.py
@@ -0,0 +1,494 @@
+# Copyright 2024 DeepMind Technologies Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Measure compositional generalization ability of LLMs."""
+
+import json
+import multiprocessing.dummy
+import os
+import timeit
+from typing import Any, Callable
+
+from absl import app
+from absl import flags
+import tensorflow as tf
+import tqdm
+
+from exedec.spec_decomposition import cached_llm_access
+from exedec.spec_decomposition import llm_utils
+
+_MODEL = flags.DEFINE_string(
+    'model', '',
+    'Which model to use.')
+_PROMPT_FORMAT = flags.DEFINE_enum(
+    'prompt_format',
+    'baseline',
+    ['baseline', 'exedec', 'exedec_ablation'],
+    'Format of the prompt to use.',
+)
+_TARGET_TASK = flags.DEFINE_enum(
+    'task',
+    None,
+    [
+        'NONE',
+        'LENGTH_GENERALIZATION',
+        'COMPOSE_DIFFERENT_CONCEPTS',
+        'SWITCH_CONCEPT_ORDER',
+        'COMPOSE_NEW_OP',
+        'EXTEND_OP_FUNCTIONALITY',
+    ],
+    'One task to run.',
+)
+
+_NUM_FEW_SHOT_EXAMPLES = flags.DEFINE_integer(
+    'num_few_shot_examples', 8,
+    'Number of few-shot examples to include in a prompt.')
+_NUM_TEST_PROBLEMS = flags.DEFINE_integer(
+    'num_test_problems', 200,
+    'Number of test problems per generalization task.')
+_NUM_SAMPLES = flags.DEFINE_integer(
+    'num_samples', 10,
+    'Number of samples to draw for one problem.')
+_TEMPERATURE = flags.DEFINE_float(
+    'temperature', 0.0, 'Temperature for the LLM.'
+)
+_NUM_WORKERS = flags.DEFINE_integer(
+    'num_workers', 72, 'Number of workers.',
+)
+
+_LLM_CACHE_DIR = flags.DEFINE_string(
+    'llm_cache_dir', '~/llm_cache',
+    'Directory for storing the LLM cache.')
+
+_VERSION_DEEPCODER = flags.DEFINE_integer(
+    'version_deepcoder', 1, 'Version of Python programs and prompts to use.'
+)
+_VERSION_ROBUSTFILL = flags.DEFINE_integer(
+    'version_robustfill', 1, 'Version of Python programs and prompts to use.'
+)
+
+DATA_FORMAT = '{dataset_dir}/{generalization_task}_data/entire_programs_{split}.tf_records-*-of-*'
+RESULTS_FORMAT = 'exedec_results/{prompt_format}_{model}_{num_test_problems}-test_{num_samples}-samples_{temperature}-temperature_deepcoder-v{version_deepcoder}_robustfill-v{version_robustfill}.json'
+MAX_NUM_DEC_STEPS = 3
+
+DatasetElement = llm_utils.DatasetElement
+ExeDecTrajectory = llm_utils.ExeDecTrajectory
+
+
+def _sample_length(dataset_type: str) -> int:
+  """Returns the maximum number of decode steps for a given dataset type."""
+  if _PROMPT_FORMAT.value == 'baseline':
+    if dataset_type == 'deepcoder':
+      return 150
+    elif dataset_type == 'robustfill':
+      return 400
+    else:
+      raise ValueError(f'Unhandled dataset type: {dataset_type}')
+  elif _PROMPT_FORMAT.value == 'exedec_ablation':
+    if dataset_type == 'deepcoder':
+      return 80
+    elif dataset_type == 'robustfill':
+      return 200
+    else:
+      raise ValueError(f'Unhandled dataset type: {dataset_type}')
+  elif _PROMPT_FORMAT.value == 'exedec':
+    if dataset_type == 'deepcoder':
+      return 200
+    elif dataset_type == 'robustfill':
+      return 400
+    else:
+      raise ValueError(f'Unhandled dataset type: {dataset_type}')
+  else:
+    raise ValueError(f'Unhandled prompt format: {_PROMPT_FORMAT.value}')
+
+
+def query_llm(
+    prompt: str,
+    n: int,
+    temperature: float,
+    model: str,
+    num_output_tokens: int | None = 512) -> list[str]:
+  """Queries an LLM with the given prompt, drawing n samples."""
+  del prompt, n, temperature, model, num_output_tokens
+  raise NotImplementedError('Call your favorite LLM here.')
+
+
+def solve_problem_baseline(
+    problem_index: int,
+    few_shot_examples: list[DatasetElement],
+    test_problem: DatasetElement,
+    dataset_type: str,
+    num_output_tokens: int,
+    verbose: bool = False,
+    ablation_style: bool = False,
+) -> dict[str, Any]:
+  """Solve a problem with baseline prompt."""
+  del ablation_style
+  start_time = timeit.default_timer()
+  if dataset_type == 'robustfill':
+    version = _VERSION_ROBUSTFILL.value
+  elif dataset_type == 'deepcoder':
+    version = _VERSION_DEEPCODER.value
+  else:
+    raise ValueError(f'Unhandled dataset type: {dataset_type}')
+  prompt = llm_utils.few_shot_prompt(
+      few_shot_examples,
+      test_problem,
+      dataset_type=dataset_type,
+      version=version,
+  )
+  samples = cached_llm_access.query_llm(
+      query_llm,
+      prompt,
+      n=_NUM_SAMPLES.value,
+      temperature=_TEMPERATURE.value,
+      model=_MODEL.value,
+      num_output_tokens=num_output_tokens,
+  )
+
+  success = False
+  for sample in samples:
+    sample = llm_utils.cut_program_from_sample(sample)
+    try:
+      outputs = llm_utils.run_program(
+          sample, test_problem.inputs, dataset_type=dataset_type
+      )
+    except Exception:  # pylint: disable=broad-exception-caught
+      outputs = None
+    if outputs == test_problem.outputs:
+      success = True
+      break
+
+  elapsed_time = timeit.default_timer() - start_time
+  result = {
+      'index': problem_index,
+      'test_problem': test_problem,
+      'samples': samples,
+      'success': success,
+      'elapsed_time': elapsed_time,
+  }
+  if verbose:
+    print(
+        f'  Test problem #{problem_index}: '
+        f'{"SUCCESS" if result["success"] else "fail"}',
+        flush=True,
+    )
+  return result
+
+
+def solve_problem_exedec(
+    problem_index: int,
+    few_shot_examples: list[DatasetElement],
+    test_problem: DatasetElement,
+    dataset_type: str,
+    num_output_tokens: int,
+    verbose: bool = False,
+    ablation_style: bool = False,
+) -> dict[str, Any]:
+  """Solve a problem with ExeDec prompt."""
+  start_time = timeit.default_timer()
+
+  samples = []
+  trajectories = []
+  success = False
+  if dataset_type == 'robustfill':
+    version = _VERSION_ROBUSTFILL.value
+  elif dataset_type == 'deepcoder':
+    version = _VERSION_DEEPCODER.value
+  else:
+    raise ValueError(f'Unhandled dataset type: {dataset_type}')
+  for _ in range(_NUM_SAMPLES.value):
+    test_problem_wo_solution = DatasetElement(
+        test_problem.inputs, test_problem.outputs, None, None
+    )
+    trajectory = []
+    for i in range(MAX_NUM_DEC_STEPS):
+      try:
+        prompt = llm_utils.few_shot_exe_dec_prompt(
+            few_shot_examples,
+            test_problem_wo_solution,
+            dataset_type=dataset_type,
+            version=version,
+            ablation_style=ablation_style,
+        )
+      except Exception:  # pylint: disable=broad-exception-caught
+        # Throws error if the previous step does not match target string in
+        # RobustFill or any other runtime error during program execution.
+        # print(e)
+        break
+      if i > 0:
+        prompt = prompt.rsplit('Putting the steps together', 1)[0]
+        if ablation_style:
+          prompt = prompt + f'Step {i + 1} code:\n'
+        else:
+          prompt = prompt + f'Step {i + 1} computes:\n'
+      if verbose:
+        print('===prompt')
+        print(prompt.rsplit('[BEGIN PROBLEM]', 1)[-1])
+
+      sample = cached_llm_access.query_llm(
+          query_llm,
+          prompt,
+          n=1,  # For step-by-step, we generate one solution at a time
+          temperature=_TEMPERATURE.value,
+          model=_MODEL.value,
+          num_output_tokens=num_output_tokens,
+      )[0]
+      program_step = llm_utils.cut_program_from_sample(sample)
+      # Record the full prediction containing the LLM-predicted subgoals.
+      # The actual outputs are not recorded but can be recomputed later.
+      trajectory.append({
+          'sample': sample,
+          'program_step': program_step,
+          'prompt': prompt,
+      })
+      if verbose:
+        print('===program_step:')
+        print(program_step)
+      # We construct a executable program for the steps generated so far.
+      program_prefix = test_problem_wo_solution.python_program
+      if dataset_type == 'deepcoder':
+        if program_prefix is None:
+          # If this is the first step:
+          # Borrow function signature from standard solution.
+          program_prefix = test_problem.python_program.splitlines()[0] + '\n'
+        else:
+          program_prefix = program_prefix.rsplit('  return', 1)[0]
+        new_var = program_step.split('=', 1)[0].strip()
+        program_suffix = f'  return {new_var}'
+      elif dataset_type == 'robustfill':
+        if program_prefix is None:
+          program_prefix = 'def program(x):\n  parts = [\n'
+        else:
+          program_prefix = program_prefix.rsplit('  ]', 1)[0]
+        program_suffix = "  ]\n  return ''.join(parts)"
+      else:
+        raise ValueError(f'Unhandled dataset type: {dataset_type}')
+      indent_spaces = '  ' if dataset_type == 'deepcoder' else '    '
+      suffix_ = ',\n' if dataset_type == 'robustfill' else '\n'
+      compose_program = (
+          program_prefix
+          + f'{indent_spaces}{program_step.strip()}{suffix_}'
+          + program_suffix
+      )
+      # Update test problem with the new program
+      test_problem_wo_solution = DatasetElement(
+          test_problem.inputs, test_problem.outputs, None, compose_program
+      )
+      if verbose:
+        print('===compose_program:')
+        print(compose_program)
+      try:
+        outputs = llm_utils.run_program(
+            compose_program, test_problem.inputs, dataset_type=dataset_type
+        )
+      except Exception:  # pylint: disable=broad-exception-caught
+        outputs = None
+      if outputs == test_problem.outputs:
+        success = True
+        break  # Stop at the first successful sample
+
+    if verbose:
+      print(test_problem_wo_solution.python_program)
+    samples.append(test_problem_wo_solution.python_program)
+    trajectories.append(trajectory)
+    if success:
+      break
+
+  elapsed_time = timeit.default_timer() - start_time
+  result = {
+      'index': problem_index,
+      'test_problem': test_problem,
+      'samples': samples,
+      'trajectories': trajectories,  # For ExeDec only
+      'success': success,
+      'elapsed_time': elapsed_time,
+  }
+  if verbose:
+    print(
+        f'  Test problem #{problem_index}: '
+        f'{"SUCCESS" if result["success"] else "fail"}',
+        flush=True,
+    )
+  return result
+
+
+def solver_parallel_run(func: Callable, inputs, num_workers: int):  # pylint: disable=g-bare-generic
+  """Run solvers in parallel with multi threading."""
+  # Run the solvers and show metrics.
+  total_cnt, pass_cnt = 0, 0
+  with multiprocessing.dummy.Pool(processes=num_workers) as p:
+    num_inputs = len(inputs)
+    results: list[Any] = [{}] * num_inputs
+    with tqdm.tqdm(total=num_inputs) as pbar:
+      for res in p.starmap(func, inputs):
+        # Update cummulated metrics.
+        total_cnt += 1
+        pass_cnt += int(res['success'])
+        pass_rate = pass_cnt / total_cnt
+        pbar.update()
+        pbar.set_postfix({
+            'pass_cnt': pass_cnt,
+            'pass_rate': pass_rate,
+            'total': total_cnt,
+        })
+        results[res['index']] = res
+  return results
+
+
+def run_experiment(
+    dataset_type: str,
+    generalization_task: str,
+    verbose: bool = False,
+    parallel: bool = True,
+) -> list[dict[str, Any]]:
+  """Runs the experiment for a generalization task."""
+  print(f'Running experiment for {dataset_type} {generalization_task}...',
+        flush=True)
+  t_start = timeit.default_timer()
+  if dataset_type == 'robustfill':
+    version = _VERSION_ROBUSTFILL.value
+  elif dataset_type == 'deepcoder':
+    version = _VERSION_DEEPCODER.value
+  else:
+    raise ValueError(f'Unhandled dataset type: {dataset_type}')
+  few_shot_dataset, test_dataset = llm_utils.load_datasets(
+      dataset_type=dataset_type,
+      generalization_task=generalization_task,
+      num_few_shot_examples=_NUM_FEW_SHOT_EXAMPLES.value,
+      num_test_problems=_NUM_TEST_PROBLEMS.value,
+      data_format=DATA_FORMAT,
+      version=version,
+  )
+  t_end = timeit.default_timer()
+  print(
+      f'Loaded {len(few_shot_dataset)} examples in {t_end - t_start} seconds',
+      flush=True,
+  )
+
+  results = []
+  num_output_tokens = _sample_length(dataset_type)
+  prompt_format = _PROMPT_FORMAT.value
+  ablation_style = prompt_format == 'exedec_ablation'
+
+  solver_inputs = []
+  for problem_index in range(_NUM_TEST_PROBLEMS.value):
+
+    test_problem = test_dataset[problem_index]
+    if version == 5:
+      few_shot_examples = few_shot_dataset
+    else:
+      few_shot_examples = llm_utils.few_shot_examples_for_test_index(
+          few_shot_dataset=few_shot_dataset,
+          test_index=problem_index,
+          num_few_shot_examples=_NUM_FEW_SHOT_EXAMPLES.value)
+      assert len(few_shot_examples) == _NUM_FEW_SHOT_EXAMPLES.value
+    solver_inputs.append([
+        problem_index,
+        few_shot_examples,
+        test_problem,
+        dataset_type,
+        num_output_tokens,
+        verbose,
+        ablation_style,
+    ])
+  if not parallel:
+    for solver_input in solver_inputs:
+      if prompt_format == 'baseline':
+        problem_result = solve_problem_baseline(*solver_input)
+      elif prompt_format == 'exedec':
+        problem_result = solve_problem_exedec(*solver_input)
+      elif prompt_format == 'exedec_ablation':
+        problem_result = solve_problem_exedec(*solver_input)
+      else:
+        raise ValueError(f'Unhandled prompt format: {prompt_format}')
+
+      results.append(problem_result)
+  else:
+    num_workers = _NUM_WORKERS.value
+    if prompt_format == 'baseline':
+      results = solver_parallel_run(
+          solve_problem_baseline, solver_inputs, num_workers
+      )
+    elif prompt_format == 'exedec':
+      results = solver_parallel_run(
+          solve_problem_exedec, solver_inputs, num_workers
+      )
+    elif prompt_format == 'exedec_ablation':
+      results = solver_parallel_run(
+          solve_problem_exedec, solver_inputs, num_workers
+      )
+    else:
+      raise ValueError(f'Unhandled prompt format: {prompt_format}')
+
+  num_success = sum(r['success'] for r in results)
+  print(f'  Solved {num_success} / {len(results)} problems', flush=True)
+  return results
+
+
+def run_entire_experiment() -> dict[str, dict[str, list[dict[str, Any]]]]:
+  """Runs the experiment for all datasets and generalization tasks."""
+  # Perform experiment.
+  all_results = {}
+  for dataset_type in ['deepcoder', 'robustfill']:
+    all_results[dataset_type] = {}
+    for generalization_task in [
+        'NONE',
+        'LENGTH_GENERALIZATION',  # Will get renamed.
+        'COMPOSE_DIFFERENT_CONCEPTS',
+        'SWITCH_CONCEPT_ORDER',
+        'COMPOSE_NEW_OP',
+        'EXTEND_OP_FUNCTIONALITY',
+    ]:
+      if _TARGET_TASK.value is not None:
+        if generalization_task != _TARGET_TASK.value:
+          continue
+      results = run_experiment(dataset_type, generalization_task, verbose=False)
+      all_results[dataset_type][generalization_task] = results
+
+  # Write actual results files.
+
+  results_path = RESULTS_FORMAT.format(
+      prompt_format=_PROMPT_FORMAT.value,
+      model=_MODEL.value.split('/')[-1],
+      num_test_problems=_NUM_TEST_PROBLEMS.value,
+      num_samples=_NUM_SAMPLES.value,
+      temperature=_TEMPERATURE.value,
+      version_deepcoder=_VERSION_DEEPCODER.value,
+      version_robustfill=_VERSION_ROBUSTFILL.value,
+  )
+  print(f'Writing results to {results_path}...', flush=True)
+  with tf.io.gfile.GFile(results_path, 'w') as f:
+    json.dump(all_results, f)
+  print('Experiment done!')
+
+  return all_results
+
+
+def main(argv) -> None:
+  if len(argv) > 1:
+    raise app.UsageError('Too many command-line arguments.')
+  if _TEMPERATURE.value == 0.0:
+    assert _NUM_SAMPLES.value == 1
+
+  cache_dir = os.path.expanduser(_LLM_CACHE_DIR.value)
+  model_name = _MODEL.value.split('/')[-1]
+  cached_llm_access.init_cache(cache_dir, model_name)
+
+  run_entire_experiment()
+
+
+if __name__ == '__main__':
+  app.run(main)
---
+++ spec_decomposition/run_llm_experiment_test.py
@@ -0,0 +1,247 @@
+# Copyright 2024 DeepMind Technologies Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Tests for run_llm_experiment.py."""
+
+import os
+from unittest import mock
+
+from absl.testing import absltest
+from absl.testing import flagsaver
+
+from exedec.spec_decomposition import cached_llm_access
+from exedec.spec_decomposition import llm_utils
+from exedec.spec_decomposition import run_llm_experiment
+
+
+DEEPCODER_1 = llm_utils.DatasetElement(
+    inputs={'x0': [[], [1, 0, 6, 9, 1], [3, 7, 1, 4]],
+            'x1': [[0], [9], [-3, -1]]},
+    outputs=[[], [5, 14, 15], [4, 5, 9]],
+    dsl_program=('x0 = INPUT | x1 = INPUT | x2 = Scanl1 (-) x0 | '
+                 'x3 = Map (*(-1)) x2 | x4 = Filter (>0) x3'),
+    python_program='''
+def program(x0, x1):
+  x2 = dsl.Scanl1(dsl.SUBTRACT, x0)
+  x3 = dsl.Map(dsl.NEGATE, x2)
+  x4 = dsl.Filter(dsl.IS_POSITIVE, x3)
+  return x4
+'''.strip(),
+)
+DEEPCODER_2 = llm_utils.DatasetElement(
+    inputs={'x3': [[1, 2, 3], [10, -10], [45]]},
+    outputs=[6, 0, 45],
+    dsl_program=None,  # Unused.
+    python_program='''
+def program(x3):
+  x7 = dsl.Sum(x3)
+  return x7
+'''.strip(),
+)
+DEEPCODER_3 = llm_utils.DatasetElement(
+    inputs={'x0': [5, 1, 2], 'x1': [[1, 2, 3], [6, 5, 4], [9, 7, 8]]},
+    outputs=[[1, 2, 3], [6], [7, 9]],
+    dsl_program=None,  # Unused.
+    python_program='''
+def program(x0, x1):
+  x2 = dsl.Take(x0, x1)
+  x3 = dsl.Sort(x2)
+  return x3
+'''.strip(),
+)
+
+ROBUSTFILL_1 = llm_utils.DatasetElement(
+    inputs=['#My##:Gxbo[Ned[Er%', '#%$Ua.Qaeq?Opa%Kcr#',
+            "%{Eos#(Mdjt#'Yi{Oclf", '%##Tq@Fh#Xza#?Fdlu'],
+    outputs=['k[MY##:GXBO[NED[ER%8y##:Gxbo[Ned[',
+             'kK%$UA.QAEQ?OPA%KCR#8aUa.Qaeq?Opa%',
+             "kO{EOS#(MDJT#'YI{OCLF8osos#(Mdjt#'Yi",
+             'kF##TQ@FH#XZA#?FDLU8qTq@Fh#Xza#?F'],
+    dsl_program=('4 29|7 109 211|3 8 111 17 109 216|'
+                 '3 15 109 216 79 7 106 216|5 219 230'),
+    python_program='''
+def program(x):
+  parts = [
+      dsl.Const('k'),
+      dsl.GetToken(x, dsl.Type.CHAR, -4),
+      dsl.ToCase(dsl.Remove(x, dsl.Type.CHAR, 1), dsl.Case.ALL_CAPS),
+      dsl.Substitute(dsl.GetToken(x, dsl.Type.PROP_CASE, 1), dsl.Type.CHAR, 1, '8'),
+      dsl.SubStr(x, 4, 15),
+  ]
+  return ''.join(parts)
+'''.strip(),
+)
+ROBUSTFILL_2 = llm_utils.DatasetElement(
+    inputs=['apple', 'banana', 'clementine', 'durian'],
+    outputs=['Apple!', 'Banana!', 'Clementine!', 'Durian!'],
+    dsl_program=None,  # Unused.
+    python_program='''
+def program(x):
+  parts = [
+      dsl.ToCase(x, dsl.Case.PROPER),
+      dsl.Const('!'),
+  ]
+  return ''.join(parts)
+'''.strip(),
+)
+ROBUSTFILL_3 = llm_utils.DatasetElement(
+    inputs=['x y', 'a b', '1 2', '! ?'],
+    outputs=['y.x', 'b.a', '2.1', '?.!'],
+    dsl_program=None,  # Unused.
+    python_program='''
+def program(x):
+  parts = [
+      dsl.GetToken(x, dsl.Type.CHAR, -1),
+      dsl.Const('.'),
+      dsl.GetToken(x, dsl.Type.CHAR, 1),
+  ]
+  return ''.join(parts)
+'''.strip(),
+)
+
+BAD_RESPONSE_1 = '''
+  dsl.NotAnOperation()
+  return None
+```
+
+More text...
+'''
+
+BAD_RESPONSE_2 = '''
+  bad indentation
+    invalid python code
+```
+
+More text...
+'''
+
+
+def _response(dataset_element: llm_utils.DatasetElement) -> str:
+  return f'{dataset_element.python_program}\n```\n\nMore text...'
+
+
+class RunLlmExperimentTest(absltest.TestCase):
+
+  @mock.patch.object(run_llm_experiment, 'query_llm')
+  @mock.patch.object(llm_utils, 'load_datasets')
+  def test_run_entire_experiment(self, mock_load_datasets, mock_query_llm):
+    # Don't actually write results to disk.
+    saved_results_format = run_llm_experiment.RESULTS_FORMAT
+    run_llm_experiment.RESULTS_FORMAT = os.path.join(
+        self.create_tempdir().full_path,
+        '{model}_{num_test_problems}-test_{num_samples}-samples.json')
+
+    model = 'mock_model'
+
+    # Use a temporary cache location.
+    cache_dir = self.create_tempdir().full_path
+    model_name = model.split('/')[-1]
+    cached_llm_access.init_cache(cache_dir, model_name)
+
+    # Check that the examples actually work.
+    for e in [DEEPCODER_1, DEEPCODER_2, DEEPCODER_3]:
+      self.assertEqual(
+          llm_utils.run_program(e.python_program, e.inputs, 'deepcoder'),
+          e.outputs)
+    for e in [ROBUSTFILL_1, ROBUSTFILL_2, ROBUSTFILL_3]:
+      self.assertEqual(
+          llm_utils.run_program(e.python_program, e.inputs, 'robustfill'),
+          e.outputs)
+
+    # load_datasets is called 6 times for the DeepCoder generalization tasks and
+    # then 6 more times for RobustFill. If we re-use a prompt, the LLM caching
+    # will lead to duplicate samples, so ensure that the prompts are different.
+    mock_load_datasets.side_effect = [
+        ([DEEPCODER_1], [DEEPCODER_2]),
+        ([DEEPCODER_1], [DEEPCODER_3]),
+        ([DEEPCODER_2], [DEEPCODER_1]),
+        ([DEEPCODER_2], [DEEPCODER_3]),
+        ([DEEPCODER_3], [DEEPCODER_1]),
+        ([DEEPCODER_3], [DEEPCODER_2]),
+
+        ([ROBUSTFILL_1], [ROBUSTFILL_2]),
+        ([ROBUSTFILL_1], [ROBUSTFILL_3]),
+        ([ROBUSTFILL_2], [ROBUSTFILL_1]),
+        ([ROBUSTFILL_2], [ROBUSTFILL_3]),
+        ([ROBUSTFILL_3], [ROBUSTFILL_1]),
+        ([ROBUSTFILL_3], [ROBUSTFILL_2]),
+    ] * 2  # *2 because we'll repeat the experiment to test caching.
+    # The corresponding 2 samples per prompt (since there is only 1 test problem
+    # per generalization task).
+    model_samples = [
+        [BAD_RESPONSE_1, BAD_RESPONSE_2],  # Failure.
+        [BAD_RESPONSE_1, _response(DEEPCODER_2)],  # Failure.
+        [_response(DEEPCODER_1), BAD_RESPONSE_1],  # Success.
+        [BAD_RESPONSE_2, _response(DEEPCODER_3)],  # Success.
+        [_response(DEEPCODER_2), BAD_RESPONSE_1],  # Failure.
+        [_response(DEEPCODER_2), BAD_RESPONSE_1],  # Success.
+
+        [_response(ROBUSTFILL_1), BAD_RESPONSE_1],  # Failure.
+        [BAD_RESPONSE_1, _response(ROBUSTFILL_3)],  # Success.
+        [BAD_RESPONSE_1, _response(ROBUSTFILL_3)],  # Failure.
+        [_response(ROBUSTFILL_2), BAD_RESPONSE_2],  # Failure.
+        [_response(ROBUSTFILL_1), BAD_RESPONSE_1],  # Success.
+        [_response(ROBUSTFILL_2), BAD_RESPONSE_2],  # Success.
+    ]
+    mock_query_llm.side_effect = model_samples
+    # Which tasks we expect to solve.
+    expected_successes = {
+        'deepcoder': ['COMPOSE_DIFFERENT_CONCEPTS',
+                      'SWITCH_CONCEPT_ORDER',
+                      'EXTEND_OP_FUNCTIONALITY'],
+        'robustfill': ['LENGTH_GENERALIZATION',
+                       'COMPOSE_NEW_OP',
+                       'EXTEND_OP_FUNCTIONALITY'],
+    }
+
+    # Run the experiment.
+    with flagsaver.flagsaver(model=model,
+                             num_few_shot_examples=1,
+                             num_test_problems=1,
+                             num_samples=2):
+      all_results = run_llm_experiment.run_entire_experiment()
+
+    # Check that we solved exactly the tasks we expected to solve.
+    for dataset_type in all_results:
+      for generalization_task in all_results[dataset_type]:
+        should_succeed = generalization_task in expected_successes[dataset_type]
+        self.assertEqual(
+            all_results[dataset_type][generalization_task][0]['success'],
+            should_succeed)
+
+    # Check that query_llm was called.
+    self.assertEqual(mock_query_llm.call_count, 12)
+
+    # Check the caching by redoing the experiment but with only bad samples.
+    mock_query_llm.reset_mock()
+    mock_query_llm.return_value = [BAD_RESPONSE_1, BAD_RESPONSE_2]
+    with flagsaver.flagsaver(model=model,
+                             num_few_shot_examples=1,
+                             num_test_problems=1,
+                             num_samples=2):
+      all_results = run_llm_experiment.run_entire_experiment()
+    for dataset_type in all_results:
+      for generalization_task in all_results[dataset_type]:
+        should_succeed = generalization_task in expected_successes[dataset_type]
+        self.assertEqual(
+            all_results[dataset_type][generalization_task][0]['success'],
+            should_succeed)
+    self.assertEqual(mock_query_llm.call_count, 0)
+
+    run_llm_experiment.RESULTS_FORMAT = saved_results_format
+
+
+if __name__ == '__main__':
+  absltest.main()
